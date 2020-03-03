2f18675be7b56137cc773c9c34c9601f829d7572
Jayesh <jayesh.patel.2197@gmail.com>
2020-03-03 14:39:46 +0530

    Add pages and templates.

---
+++ src/pages/contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Typography from '@material-ui/core/Typography'
+import Divider from '@material-ui/core/Divider'
+
+const Contact = props => (
+  <div>
+    <Typography variant="h3">Contact</Typography>
+    <Divider />
+    <Typography variant="subtitle1">Under Construction</Typography>
+  </div>
+)
+
+export default Contact
