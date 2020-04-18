6a9ac8a1279fbd24ae19cbf36aa541bac490554e
Isank <isankagarwal@gmail.com>
2020-04-18 22:01:43 +0530

    extract menu toggle functionality to separate JS file

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "css/processed": "css" });
   eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.addPassthroughCopy("js");
 };
