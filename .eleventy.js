e56e06de888b3af38bc0ba66125fc15657c7d876
Isank <isankagarwal@gmail.com>
2020-04-22 02:03:43 +0530

    add markdown it and highlight js

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,21 @@
 module.exports = function (eleventyConfig) {
+  let highlightJs = require("highlight.js");
+  let markdownIt = require("markdown-it");
+  let markdownOptions = {
+    html: true,
+    highlight: function (str, lang) {
+      if (lang && highlightJs.getLanguage(lang)) {
+        try {
+          return highlightJs.highlight(lang, str).value;
+        } catch (__) {}
+      }
+
+      return ""; // use external default escaping
+    },
+  };
+
+  eleventyConfig.setLibrary("md", markdownIt(markdownOptions));
+
   eleventyConfig.addPassthroughCopy({ "css/processed": "css" });
   eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addPassthroughCopy("js");
